1fcda41d74e4a33596245396cb5ad03c255145c3
juxsalley <juxsalley@gmail.com>
2021-02-06 02:11:07 +0000

    add two D Translate

---
+++ two_d_translate.rb
@@ -0,0 +1,26 @@
+def two_d_translate(arr)
+  res = []
+  arr.each do |ele|
+    ele[1].times {res.push(ele[0])}
+  end
+  res
+end
+
+
+arr_1 = [
+  ['boot', 3],
+  ['camp', 2],
+  ['program', 0]
+]
+
+print two_d_translate(arr_1) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+
+arr_2 = [
+  ['red', 1],
+  ['blue', 4]
+]
+
+print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
+
